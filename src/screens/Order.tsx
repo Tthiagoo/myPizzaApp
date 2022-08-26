b3bfb0bc119d7d2741a89ded5cf8f5bfcd9bb598
Thiago Pereira <51219408+Tthiagoo@users.noreply.github.com>
2022-08-26 18:44:28 -0300

    ajustes order

--- src/screens/Order.tsx
+++ src/screens/Order.tsx
@@ -13,75 +13,71 @@ import {
   FormControl,
   Input,
   WarningOutlineIcon,
-  ScrollView,
-  KeyboardAvoidingView
+  KeyboardAvoidingView,
+  ScrollView
 } from 'native-base'
 
 import React from 'react'
-
+import { Platform } from 'react-native'
 export default function PizzaOrder() {
   return (
-    <KeyboardAvoidingView flex={1} bg="light.200">
-      <ScrollView borderColor="black" borderWidth={3}>
-        <Flex width="100%" height={'40%'} bg="red.700">
+    <KeyboardAvoidingView
+      flex="1"
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+    >
+      <ScrollView bg="light.200">
+        <Flex bg="red.700" height={'200'} safeAreaTop>
           <ArrowBackIcon size="6" mt="8%" ml="5%" color="white" />
         </Flex>
         <Image
           alt="pizza"
           rounded={'full'}
           size="2xl"
-          top={'-30%'}
+          top={'-120px'}
           source={require('../../assets/pizza2.png')}
           alignSelf="center"
         />
-        <Heading alignSelf={'center'} top="-13%">
-          Magherita
-        </Heading>
-
-        <Box h="40%">
-          <Text ml="3%">Selecione um tamanho</Text>
+        <Flex px="20px" bg="blue.100" top="-100px" h="400">
+          <Heading alignSelf={'center'}>Magherita</Heading>
+          <Text>Selecione um tamanho</Text>
           <Radio.Group
             defaultValue="1"
             name="myRadioGroup"
             accessibilityLabel="Pick your favorite number"
           >
-            <HStack w="100%" mt="4%" justifyContent="space-around">
-              <Flex
+            <HStack w="100%" mt="4%" justifyContent="space-between">
+              <VStack
                 bg="light.300"
                 w="25%"
+                padding={'10px'}
                 justifyContent={'center'}
                 alignItems="center"
                 borderRadius={'10px'}
               >
-                <Radio value="1" my={1}>
-                  <Text>First</Text>
-                </Radio>
-              </Flex>
+                <Radio value="1" my={1} accessibilityLabel="Radio" />
+                <Text>Pequena</Text>
+              </VStack>
 
-              <Flex
+              <VStack
                 bg="light.300"
                 w="25%"
-                h="100%"
                 justifyContent={'center'}
                 alignItems="center"
                 borderRadius={'10px'}
               >
-                <Radio value="2" my={1}>
-                  <Text>First</Text>
-                </Radio>
-              </Flex>
-              <Flex
+                <Radio value="1" my={1} accessibilityLabel="Radio" />
+                <Text>Media</Text>
+              </VStack>
+              <VStack
                 bg="light.300"
                 w="25%"
-                h="100%"
                 justifyContent={'center'}
                 alignItems="center"
                 borderRadius={'10px'}
               >
-                <Radio value="3" my={1}>
-                  <Text>First</Text>
-                </Radio>
-              </Flex>
+                <Radio value="1" my={1} accessibilityLabel="Radio" />
+                <Text fontWeight={'bold'}>Grande</Text>
+              </VStack>
             </HStack>
             <HStack w="100%">
               <Box alignItems="center">
@@ -92,7 +88,7 @@ export default function PizzaOrder() {
               </Box>
             </HStack>
           </Radio.Group>
-        </Box>
+        </Flex>
       </ScrollView>
     </KeyboardAvoidingView>
   )
