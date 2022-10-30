e446b8de6bab93853a0e8d4b2390f84e37f69f13
Tthiagoo <51219408+Tthiagoo@users.noreply.github.com>
2022-10-29 21:32:53 -0300

    validação form cadastro usuario

--- src/components/InputForm.tsx
+++ src/components/InputForm.tsx
@@ -1,4 +1,9 @@
-import { IInputProps, Input as NativeInput } from 'native-base'
+import {
+  FormControl,
+  IInputProps,
+  Input as NativeInput,
+  Text
+} from 'native-base'
 import React from 'react'
 
 type Props = IInputProps & {
@@ -12,20 +17,35 @@ export default function Input({
 }: Props) {
   const invalid = !!errorMessage || isInvalid
   return (
-    <NativeInput
-      placeholderTextColor={'white'}
-      color="muted.50"
-      size="md"
-      isInvalid={invalid}
-      marginBottom={'5%'}
-      borderColor="light.100"
-      borderWidth={'0.3'}
-      variant="unstyled"
-      _focus={{
-        borderColor: 'white',
-        backgroundColor: '#ffffff3e'
-      }}
-      {...rest}
-    />
+    <>
+      <NativeInput
+        placeholderTextColor={'white'}
+        color="muted.50"
+        size="md"
+        isInvalid={invalid}
+        marginBottom={'5%'}
+        borderColor="light.100"
+        borderWidth={'0.3'}
+        variant="unstyled"
+        _focus={{
+          borderColor: 'white',
+          backgroundColor: '#ffffff3e'
+        }}
+        {...rest}
+        _invalid={{
+          borderWidth: 2,
+          borderColor: 'red.500'
+        }}
+      />
+
+      <Text
+        marginTop="-8px"
+        marginBottom={'8px'}
+        display={invalid ? 'flex' : 'none'}
+        color="red.500"
+      >
+        {errorMessage}
+      </Text>
+    </>
   )
 }
--- src/screens/RegisterUser.tsx
+++ src/screens/RegisterUser.tsx
@@ -1,5 +1,16 @@
 import { useNavigation } from '@react-navigation/native'
-import { Button, Flex, Heading, HStack, VStack, Text } from 'native-base'
+import * as yup from 'yup'
+import { yupResolver } from '@hookform/resolvers/yup'
+
+import {
+  Button,
+  Flex,
+  Heading,
+  HStack,
+  VStack,
+  Text,
+  FormControl
+} from 'native-base'
 import React, { useState } from 'react'
 import {
   signInWithEmailAndPassword,
@@ -10,55 +21,58 @@ import { auth, db } from '../config/firebase'
 import { addDoc, collection } from 'firebase/firestore'
 import { Alert } from 'react-native'
 import Input from '../components/InputForm'
+import { useForm, Controller } from 'react-hook-form'
 
 export default function RegisterUser() {
-  const [name, setName] = useState('')
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const [cpf, setCpf] = useState('')
-  const [apto, setApto] = useState('')
-  const [bloco, setBloco] = useState('')
-
   const [loading, setLoading] = useState(Boolean)
 
   const navigation = useNavigation()
   const userRef = collection(db, 'Users')
 
-  const handleUserRegistration = async () => {
-    if (!name.trim()) {
-      return Alert.alert('Cadastro', 'Informe o seu da nome.')
-    }
+  const schema = yup.object({
+    name: yup
+      .string()
+      .required('Informe um nome')
+      .min(4, 'Minimo de 4 digitos')
+      .matches(/^[aA-zZ\s]+$/, 'Somente letras é aceito'),
+    password: yup.string().required('Informe uma senha'),
+    email: yup
+      .string()
+      .required('Informe um e-mail')
+      .email('Digite um email valido'),
+    cpf: yup
+      .string()
+      .required('Informe um cpf')
+      .min(11, 'Informe um cpf valido')
+      .matches(/^[0-9]+$/, 'Somente numeros'),
+    apto: yup.string().required('Informe um apto'),
+    bloco: yup.number().required('Informe um bloco')
+  })
 
-    if (!email.trim()) {
-      return Alert.alert('Cadastro', 'Informe o seu e-mail')
-    }
+  const {
+    handleSubmit,
+    control,
+    formState: { errors }
+  } = useForm<schemaTypeValidation>({ resolver: yupResolver(schema) })
 
-    if (!password) {
-      return Alert.alert('Cadastro', 'Informe a sua senha')
-    }
-    if (!cpf.trim()) {
-      return Alert.alert('Cadastro', 'Informe o seu CPF')
-    }
-    if (!apto.trim()) {
-      return Alert.alert('Cadastro', 'Informe o seu apartamento.')
-    }
+  type schemaTypeValidation = yup.InferType<typeof schema>
 
-    if (!bloco) {
-      return Alert.alert('Cadastro', 'Informe o seu bloco')
-    }
-
+  const handleUserRegistration = async (data: schemaTypeValidation) => {
     try {
       setLoading(true)
-      const res = await createUserWithEmailAndPassword(auth, email, password)
+      const res = await createUserWithEmailAndPassword(
+        auth,
+        data.email,
+        data.password
+      )
       const user = res.user
       await addDoc(userRef, {
         uid: user.uid,
-        name,
-        authProvider: 'local',
-        email,
-        apto,
-        bloco,
-        cpf
+        name: data.name,
+        email: data.email,
+        apto: data.apto,
+        bloco: data.bloco,
+        cpf: data.cpf
       }).then(() => {
         setLoading(false)
         Alert.alert('Cadastro', 'Cadastro concluido com sucesso')
@@ -85,32 +99,98 @@ export default function RegisterUser() {
       justifyContent="center"
     >
       <Heading color="white">Faça seu cadastro</Heading>
-      <VStack w="100%" mt="5" space={3}>
-        <Input onChangeText={setName} placeholder="Nome" />
-        <Input placeholder="E-mail" onChangeText={setEmail} />
-        <Input placeholder="Senha" onChangeText={setPassword} />
-        <Input placeholder="CPF" onChangeText={setCpf} />
-        <HStack w="100%" space={3} paddingX="1" justifyContent="center">
-          <Input w="50%" placeholder="APTO" onChangeText={setApto} />
-          <Input w="50%" placeholder="Bloco" onChangeText={setBloco} />
-        </HStack>
-        <Button
-          onPress={handleUserRegistration}
-          isLoading={loading}
-          _pressed={{ opacity: 0.6 }}
-          style={{
-            borderRadius: 12,
-            maxHeight: 50,
-            minHeight: 50,
-            justifyContent: 'center',
-            alignItems: 'center',
-            backgroundColor: '#e73b49',
-            flex: 1
-          }}
-        >
-          <Text color={'white'}> Cadastar </Text>
-        </Button>
-      </VStack>
+
+      <FormControl>
+        <VStack w="100%" mt="5" space={3}>
+          <Controller
+            control={control}
+            name="name"
+            render={({ field: { onChange } }) => (
+              <Input
+                onChangeText={onChange}
+                errorMessage={errors.name?.message}
+                placeholder="Nome"
+              />
+            )}
+          />
+          <Controller
+            control={control}
+            name="email"
+            render={({ field: { onChange } }) => (
+              <Input
+                onChangeText={onChange}
+                errorMessage={errors.email?.message}
+                placeholder="E-mail"
+              />
+            )}
+          />
+          <Controller
+            control={control}
+            name="password"
+            render={({ field: { onChange } }) => (
+              <Input
+                onChangeText={onChange}
+                errorMessage={errors.password?.message}
+                placeholder="Senha"
+              />
+            )}
+          />
+          <Controller
+            control={control}
+            name="cpf"
+            render={({ field: { onChange } }) => (
+              <Input
+                onChangeText={onChange}
+                errorMessage={errors.cpf?.message}
+                placeholder="CPF"
+              />
+            )}
+          />
+
+          <HStack w="100%" space={3} paddingX="1" justifyContent="center">
+            <Controller
+              control={control}
+              name="apto"
+              render={({ field: { onChange } }) => (
+                <Input
+                  onChangeText={onChange}
+                  errorMessage={errors.apto?.message}
+                  w="50%"
+                  placeholder="APTO"
+                />
+              )}
+            />
+            <Controller
+              control={control}
+              name="bloco"
+              render={({ field: { onChange } }) => (
+                <Input
+                  onChangeText={onChange}
+                  errorMessage={errors.bloco?.message}
+                  w="50%"
+                  placeholder="Bloco"
+                />
+              )}
+            />
+          </HStack>
+          <Button
+            onPress={handleSubmit(handleUserRegistration)}
+            isLoading={loading}
+            _pressed={{ opacity: 0.6 }}
+            style={{
+              borderRadius: 12,
+              maxHeight: 50,
+              minHeight: 50,
+              justifyContent: 'center',
+              alignItems: 'center',
+              backgroundColor: '#e73b49',
+              flex: 1
+            }}
+          >
+            <Text color={'white'}> Cadastar </Text>
+          </Button>
+        </VStack>
+      </FormControl>
     </Flex>
   )
 }
